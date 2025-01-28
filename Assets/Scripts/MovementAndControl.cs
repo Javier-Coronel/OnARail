41f98e184412ba88a297422a038c2d73fccd6877
Javier-Coronel <90841478+Javier-Coronel@users.noreply.github.com>
2025-01-28 18:29:38 +0100

    movement added

--- Assets/Scripts/MovementAndControl.cs
+++ Assets/Scripts/MovementAndControl.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class MovementAndControl : MonoBehaviour
 {
     public GameObject mainTrack;
-
+    public Animator cartAnimator;
     public float maxMovementOnX=5;
     public float maxMovementOnZ=5;
     public float velocity = 1;
+    private Vector3 onDashDirection;
+    public float dashTime=1;
+    public float dashSpeed=1;
+    private float dashTimer=0;
     private GameObject leftHandObject = null;
     private GameObject rightHandObject = null;
+    private float xMovement;
+    private float zMovement;
+    private bool dash;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +26,11 @@ public class MovementAndControl : MonoBehaviour
     }
     void Update()
     {
+        xMovement = Input.GetAxis("Horizontal");
+        zMovement = Input.GetAxis("Vertical");
+        dash = Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return); 
+        Vector3 movement = new Vector3(xMovement, 0, zMovement).normalized * velocity;
+        //transform.parent.rotation = Quaternion.LookRotation(transform.position, transform.position+movement);
         if (transform.localPosition.x > maxMovementOnX)
         {
             transform.localPosition = new Vector3(maxMovementOnX, transform.localPosition.y, transform.localPosition.z);
@@ -34,18 +47,28 @@ public class MovementAndControl : MonoBehaviour
         {
             transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, -maxMovementOnZ);
         }
-    }
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        float xMovement = Input.GetAxis("Horizontal");
-        float zMovement = Input.GetAxis("Vertical");
-        bool dash = Input.GetKeyDown(KeyCode.Space);
-        Vector3 movement = new Vector3(xMovement, 0, zMovement).normalized * velocity;
-        transform.Translate(movement);
-        if (dash)
+        if (onDashDirection.x != 0f || onDashDirection.z != 0f)
         {
-
+            dashTimer += Time.deltaTime;
+            if (dashTimer >= dashTime)
+            {
+                dashTimer = 0;
+                GetComponent<MeshRenderer>().materials[0].color = Color.white;
+                onDashDirection.x = 0;
+                onDashDirection.z = 0;
+            }
+            else
+            {
+                transform.Translate(onDashDirection*Time.deltaTime);
+            }
+        }
+        else if (dash)
+        {
+            Dash(movement);
+        }
+        else
+        {
+            transform.Translate(movement * Time.deltaTime);
         }
     }
     void OnTriggerStay(Collider other)
@@ -89,6 +112,14 @@ public class MovementAndControl : MonoBehaviour
     }
     void Dash(Vector3 direction)
     {
-
+        if (direction.x == 0f && direction.z == 0f)
+        {
+            direction=Vector3.forward*velocity;
+        }
+        cartAnimator.SetTrigger("Dash");
+        GetComponent<MeshRenderer>().materials[0].color = Color.black;
+        onDashDirection = direction*dashSpeed;
+        Debug.Log(direction);
+        Debug.Log(onDashDirection);
     }
 }
