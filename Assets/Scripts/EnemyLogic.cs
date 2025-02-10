e111eb487501ea1963e3b0d7ee880074ba4fd8b4
Javier-Coronel <alum.jcoronelo.2021@iesalixar.org>
2025-02-10 05:30:47 +0100

    Tryng to add a physical representation of the enemy detection zone

--- Assets/Scripts/EnemyLogic.cs
+++ Assets/Scripts/EnemyLogic.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.U2D;
 
 public class EnemyLogic : MonoBehaviour
 {
     int searchLayer;
     public float angle = 10;
     public GameObject physicalEnemy;
+    public SpriteShapeController detectionZone;
     public float maxDistance = 5;
     private Vector3 leftMod;
     private Vector3 rightMod;
@@ -14,24 +16,38 @@ public class EnemyLogic : MonoBehaviour
     void Start()
     {
         searchLayer = GameData.Instance.playerCart.gameObject.layer;
-        leftMod = new Vector3(0, 0, Mathf.Sin(angle));
-        rightMod = new Vector3(0, 0, Mathf.Sin(-angle));
+        leftMod = physicalEnemy.transform.right*Mathf.Sin(angle);
+        rightMod = physicalEnemy.transform.right*Mathf.Sin(-angle);
+        detectionZone.spline.SetPosition(1, (physicalEnemy.transform.up + rightMod) * maxDistance);
+        detectionZone.spline.SetPosition(2, physicalEnemy.transform.up * (maxDistance+1.25f));
+        detectionZone.spline.SetPosition(3, (physicalEnemy.transform.up + leftMod) * maxDistance);
+        detectionZone.spline.SetRightTangent(1, -physicalEnemy.transform.right);
+        detectionZone.spline.SetLeftTangent(1,  physicalEnemy.transform.right);
+        detectionZone.spline.SetRightTangent(2, -physicalEnemy.transform.right);
+        detectionZone.spline.SetLeftTangent(2,  physicalEnemy.transform.right);
+        detectionZone.spline.SetRightTangent(3, -physicalEnemy.transform.right);
+        detectionZone.spline.SetLeftTangent(3,  physicalEnemy.transform.right);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        leftMod = new Vector3(0, 0, Mathf.Sin(angle));
-        rightMod = new Vector3(0, 0, Mathf.Sin(-angle));
-        Physics.Raycast(physicalEnemy.transform.position, (physicalEnemy.transform.right+ leftMod), out RaycastHit leftHit, maxDistance, searchLayer);
-        Physics.Raycast(physicalEnemy.transform.position, (physicalEnemy.transform.right+ rightMod), out RaycastHit rightHit, maxDistance, searchLayer);
-        Physics.Raycast(physicalEnemy.transform.position, physicalEnemy.transform.right, out RaycastHit centerHit, maxDistance, searchLayer);
-        Debug.DrawRay(physicalEnemy.transform.position, physicalEnemy.transform.right * maxDistance, Color.white);
-        Debug.DrawRay(physicalEnemy.transform.position, (physicalEnemy.transform.right + leftMod) * maxDistance, Color.white);
-        Debug.DrawRay(physicalEnemy.transform.position, (physicalEnemy.transform.right + rightMod) * maxDistance, Color.white);
-        Debug.Log(physicalEnemy.transform.right);
-        Debug.Log((physicalEnemy.transform.right + leftMod));
-        Debug.Log((physicalEnemy.transform.right + rightMod));
+        leftMod = physicalEnemy.transform.right*Mathf.Sin(angle);
+        rightMod = physicalEnemy.transform.right*Mathf.Sin(-angle);
+        bool left = Physics.Raycast(physicalEnemy.transform.position, (physicalEnemy.transform.forward + leftMod), out RaycastHit leftHit, maxDistance, searchLayer);
+        bool right = Physics.Raycast(physicalEnemy.transform.position, (physicalEnemy.transform.forward + rightMod), out RaycastHit rightHit, maxDistance, searchLayer);
+        bool center = Physics.Raycast(physicalEnemy.transform.position, physicalEnemy.transform.forward, out RaycastHit centerHit, maxDistance, searchLayer);
+        Debug.Log("PlayerDetected " + left + " " + right + " " + center);
+        if(left||right||center){
+            Debug.Log("PlayerDetected " + leftHit + " " + rightHit + " " + centerHit);
+        }
+        Debug.DrawRay(physicalEnemy.transform.position,  physicalEnemy.transform.forward * maxDistance, Color.white);
+        Debug.DrawRay(physicalEnemy.transform.position, (physicalEnemy.transform.forward + leftMod) * maxDistance, Color.blue);
+        Debug.DrawRay(physicalEnemy.transform.position, (physicalEnemy.transform.forward + rightMod) * maxDistance, Color.red);
+
+        //Debug.Log( physicalEnemy.transform.forward);
+        //Debug.Log((physicalEnemy.transform.forward + leftMod));
+        //Debug.Log((physicalEnemy.transform.forward + rightMod));
 
     }
 }
